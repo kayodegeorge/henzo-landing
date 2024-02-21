1b7fdacf7851caa7ec17d262893bc18af9bb9ea8
Kayode George <gkayode75@gmail.com>
2024-02-21 11:14:36 +0100

    barebones landing

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className='bg-primary-green py-10 px-[200px] flex justify-between'>
+      <h1 className='font-semibold text-2xl'>Terms of Service</h1>
+      <h1 className='font-semibold text-2xl'>Privacy Policy</h1>
+    </footer>
+  )
+}
+
+export default Footer
