d47f28c0b400de39ade50fc910cfcf85bcd505c2
kayodegeorge <kayodegeorge>
2024-04-24 19:01:50 +0100

    feat: update sm links and hamburger

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
         href="/contact"
       ></Link>
 
-      {/* <Sheet open={open} onOpenChange={setOpen}>
+      <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger>
           <div className="hidden h-[3.75rem] w-[3.75rem] items-center justify-center rounded-full bg-primary-green/30 lg:flex">
             <AlignRight className="h-6 w-6" />
@@ -117,13 +117,13 @@ export default function Navbar() {
             </div>
           </div>
         </SheetContent>
-      </Sheet> */}
-      <Link
-        href="https://facebook.com"
+      </Sheet>
+      {/* <Link
+        href="https://info@henzologistics.com"
         className="text-primary-green underline"
       >
         Contact us
-      </Link>
+      </Link> */}
     </nav>
   );
 }
